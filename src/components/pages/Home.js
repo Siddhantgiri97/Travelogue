967727c927b30cb1e6d1a69b207b215f3669c058
Siddhant <siddhantgiri97@gmail.com>
2022-07-16 10:46:55 +0530

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import '../../App.css'
+import HeroSection from '../HeroSection'
+import Cards from '../Cards'
+import Footer from '../Footer'
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
